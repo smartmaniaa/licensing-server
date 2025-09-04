b4ccf53aa5a4d502017428db0fbf2b1e98450a6d
smartmaniaa <smart@maniaa.com.br>
2025-09-04 11:19:00 -0300

    ajustes

--- stripe_config.rb
+++ stripe_config.rb
@@ -3,5 +3,4 @@ require 'stripe'
 
 # Define a chave secreta da API para toda a aplicação.
 # Trate esta chave como uma senha!
-#Stripe.api_key = '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_AQUI' # <<<<<<< COLOQUE SUA CHAVE AQUI
 Stripe.api_key = ENV['STRIPE_API_KEY']
